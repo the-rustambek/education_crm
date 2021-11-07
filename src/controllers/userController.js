f84abdd582f1bd98ed81ad6549cc3a8864f549a6
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-11-07 17:47:26 +0500

    add validations

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,9 @@
+const { signInValidation } = require("../modules/validation");
+
 module.exports =  class userController{
     static async signInController(req,res,next){
          try {
-             const {username,password} = req.body;
+             const {username,password} =await signInValidation(req.body);
          } catch (error) {
              
          }
--- src/modules/validation.js
+++ src/modules/validation.js
@@ -1,3 +1,23 @@
 const joi = require("joi");
 
-module.exports = class Validations
+module.exports = class Validations{
+    static async signUpValidation(data){
+        return await joi.object({
+            name: joi.string().required().min(5).max(64).error(new Error("Name is invalid")),
+            email:joi.string().email().error(new Error("Email is invalid")),
+            password: joi.string().required().max(128).min(6).error(new Error("Password is invalid")),
+            username: joi.string().regex(/^[A-Za-z]{2,}[_-]?[A-Za-z0-9]{2,}$/).required.error(new Error("username is invalid")),
+            gender:joi.string().required().valid("male","female").error(new Error("This option isn't available")),
+        }).validateAsync(data);
+    }
+
+
+    static async signInValidation(data){
+        return await joi.object({
+            password: joi.string().required().max(128).min(6).error(new Error("Password is invalid")),
+            username: joi.string().regex(/^[A-Za-z]{2,}[_-]?[A-Za-z0-9]{2,}$/).required.error(new Error("Username is invalid")),
+        }).validateAsync(data);
+    }
+
+
+}
