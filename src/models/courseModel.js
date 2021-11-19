72c47b9ee922564f156205b713e2413067a84674
Rustambek Mamadaliyev <80699038+Rustambek-Mamadaliyev0301@users.noreply.github.com>
2021-11-19 11:15:00 +0500

    add courseModel.js

---
+++ src/models/courseModel.js
@@ -0,0 +1,28 @@
+module.exports = async (sequelize, Sequelize) =>{
+    return await sequelize.define("courses",{
+        course_id:{
+            type:Sequelize.UUID,
+            defaultValue: Sequelize.UUIDV4(),
+            allowNull: false,
+            primaryKey:true,
+        },
+        course_name:{
+            type:Sequelize.STRING(64),
+            allowNull:false,
+        },
+        course_price:{
+            type:Sequelize.INTEGER,
+            allowNull:false,
+            defaultValue:0,
+        },
+        course_description:{
+            type:Sequelize.TEXT,
+            allowNull:false
+        },
+        course_photo:{
+            type: Sequelize.STRING,
+            allowNull:true,
+        }
+
+    })
+}
